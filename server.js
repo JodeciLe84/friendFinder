e09afaf639105e02a3fe87f79d9f9df594e26dc8
jodecile84 <jodecile84@gmail.com>
2018-04-23 21:47:43 -0700

    another

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ var PORT = process.env.port || 3000
 //Middle Wares
 app.use(express.static(path.join(__dirname + '/app/public')))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Requireing Routes form the path. // api route first
 require('./app/routing/apiRoutes.js')(app)
 require('./app/routing/htmlRoutes.js')(app)
 
 //Starting Server
-app.listen(PORT, function() {
-  console.log('Server listening on http://localhost:'+ PORT)
+app.listen(PORT, function () {
+  console.log('Server listening on http://localhost:' + PORT)
 })
